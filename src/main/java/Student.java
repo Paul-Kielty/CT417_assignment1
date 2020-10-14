864944aab16a2e9ca27cbd7f0fc7da633907e26c
Paul-Kielty <p.kielty3@nuigalway.ie>
2020-10-14 17:06:18 +0100

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Student {
+    private String name, DOB;
+    private int age;
+
+
+    public Student(String name, int age, String DOB){//, int ID, String username, ArrayList courses, ArrayList modules){
+        this.name = name;
+        this.age = age;
+        this.DOB = DOB;
+
+    }
+}
