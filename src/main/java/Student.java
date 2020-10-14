b3bb9a517dc2d45b6a3660b4be291176019b2a89
Paul-Kielty <p.kielty3@nuigalway.ie>
2020-10-14 17:38:22 +0100

    added Student attributes and getters + setters

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,14 +1,53 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Student {
-    private String name, DOB;
-    private int age;
+    private String name, DOB, username;
+    private int age, ID;
 
-
-    public Student(String name, int age, String DOB){//, int ID, String username, ArrayList courses, ArrayList modules){
+    public Student(String name, int age, String DOB, int ID, String username){
         this.name = name;
         this.age = age;
         this.DOB = DOB;
+        this.username = getUsername();
+    }
 
+    public String getUsername(){
+        return name+age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
     }
 }
