a519333b80db6647173f6b421fd92b4215a51f29
Paul-Kielty <p.kielty3@nuigalway.ie>
2020-11-13 11:02:50 +0000

    Created Module class

---
+++ src/main/java/Module.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+public class Module {
+
+    private String moduleName;
+    private int moduleID;
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+//    private ArrayList<Course> courseList = new ArrayList<Course>();
+
+
+    public Module(String moduleName, int moduleID, ArrayList studentList) {
+        this.moduleName = moduleName;
+        this.moduleID = moduleID;
+//        this.studentList = studentList;
+    }
+
+    public void addStudent(Student s) {
+        studentList.add(s);
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public int getModuleID() {
+        return moduleID;
+    }
+
+    public void setModuleID(int moduleID) {
+        this.moduleID = moduleID;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+}
